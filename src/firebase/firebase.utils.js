d7437dbc3e1aca434069560e7d10fdfca69cdb2d
Dardan Lladrovci <dardanlladrovci@gmail.com>
2020-04-29 00:48:26 +0200

    Store authorized user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,13 +13,39 @@ const config = {
   measurementId: "G-QG3MX8LSTW"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('Error creating user', error.message)
+    }
+  }
+
+  return userRef;
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-// export const firestore = firestore.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ promt: 'select_acount' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
